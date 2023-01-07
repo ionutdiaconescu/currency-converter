ae854a59764a683161aaa2628d5a19ddf7769f3f
ionutdiaconescu <ionutdiaconescu95@yahoo.com>
2023-01-07 15:15:09 +0200

    no commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import React, { useEffect, useState} from 'react';
+import { isCompositeComponent } from 'react-dom/test-utils';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
 
-function App() {
+
+
+
+const BASE_URL = 'https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=USD&to=EUR%2CGBP';
+
+const apikey = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd409502676msh9e33d601204db7ep110138jsnd60d3c4e5317',
+    'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
+  }
+};
+
+function App (){
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true) 
+
+ let toAmount, fromAmount
+ if (amountInFromCurrency) {
+  fromAmount = amount
+  toAmount = amount * exchangeRate
+ } else {
+  toAmount = amount
+  fromAmount = amount / exchangeRate
+ }
+    useEffect(() =>{
+      fetch(BASE_URL, apikey)
+        .then(res => res.json())
+        .then(data => {
+          const firstCurrency = Object.keys(data.rates)[0]
+          setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+          setFromCurrency(data.base)
+          setToCurrency(firstCurrency)
+          setExchangeRate(data.rates[firstCurrency])
+        })
+        
+    }, [])
+
+    useEffect(() => {
+        if (fromCurrency != null && toCurrency != null) {
+          fetch(`${BASE_URL, apikey}?base=${fromCurrency}$symbols=${toCurrency}`)
+          .then(res => res.json())
+          .then(data => setExchangeRate(data.rates[toCurrency]))
+        }
+    }, [fromCurrency, toCurrency])
+  
+function handleFromAmountChange(e) {
+setAmount(e.target.value)
+setAmountInFromCurrency(true)
+}
+function handleToAmountChange(e) {
+  setAmount(e.target.value)
+  setAmountInFromCurrency(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <h1>Convert</h1>
+   <CurrencyRow 
+   currencyOptions={currencyOptions}
+   selectedCurrency={fromCurrency}
+   onChangeCurrency={e => setFromCurrency(e.target.value)}
+   onChangeAmount={handleFromAmountChange}
+   amount={fromAmount}
+   />
+   <div className='equals'>=</div>
+   <CurrencyRow
+   currencyOptions={currencyOptions}
+   selectedCurrency={toCurrency}
+   onChangeCurrency={e => setToCurrency(e.target.value)}
+   onChangeAmount={handleToAmountChange}
+   amount={toAmount}
+   />
+
+   </>
   );
 }
 
